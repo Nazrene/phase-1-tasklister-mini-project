a8709d0a7e017cd07c9e4f653ef8dfa8d6f385bd
Naz <mercy.kamau@student.moringaschool.com>
2023-10-11 14:48:48 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+const tasks = [
+  {description: 'Record stock' },
+  { description: 'Order purchase' },
+  { description: 'Inventory' },
+  { description: 'Balance books' }
+]; 
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault(); 
+  const taskDescription = document.getElementById('new-task-description').value;
+  
+});
+const tasksList = document.getElementById('tasks-list'); 
+
+function displayTasks() {
+  
+  tasksList.innerHTML = '';
+
+  tasks.forEach((task, index) => {
+    const listItem = document.createElement('li');
+    listItem.textContent = `${index + 1}. ${task.description}`; 
+    tasksList.appendChild(listItem); 
+  });
+}
+ displayTasks(); 
+ 
+document.querySelector('h2').addEventListener('click', displayTasks);
+
+
+
 });
